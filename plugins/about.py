c2ea2da7480762489788301e85de4c75ce0ea90d
Rahul <144818724+Ragesg@users.noreply.github.com>
2025-07-22 17:36:38 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -6,10 +6,10 @@ INFO_TEXT = (
     "🧾 *About Me*\n\n"
     "👤 *Owner:* [Achhaaa 🙈](https://t.me/rahulp_r)\n"
     "👥 *Total Users:* അറിഞ്ഞിട്ട് എന്തിനാ 😂...\n"
-    "🖥️ *Server:* Free Server Alla But Down ആയേക്കാം ⚡️\n"
+    "🖥️ *Server:* Paid , But Down ആയേക്കാം ⚡️\n"
     "🧠 *Memory:* 1 GB 😧\n"
-    "📅 *Uptime:* Born on 29th Jan 👶\n"
-    "🧾 *Bot Version:* v3.1.7 [Beta]"
+    "📅 *DOB:* Born on 29th Jan 👶\n"
+    "🧾 *Bot Version:* v3.1.7 \\[Beta\\]"
 )
 
 # Responds to /about or /info
@@ -17,7 +17,7 @@ async def about_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton("🔙 Back", callback_data="main_menu")]]
     await update.message.reply_text(INFO_TEXT, parse_mode="Markdown", reply_markup=InlineKeyboardMarkup(keyboard))
 
-# Handles button press for "ℹ️ Info"
+# Handles button press for "😜 About Me"
 async def info_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
